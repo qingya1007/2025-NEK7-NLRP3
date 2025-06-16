97faab9d909061dc584914cb78da2aa4792ff568
qingya1007 <linqingya1007@163.com>
2025-06-16 16:22:43 +0800

    Create distance.sh

---
+++ inactive_without_NEK7/distance/distance.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cat>distance.in<<EOD
+parm /home/jinp/project/NLRP3/inactive_no_NEK7/MD1/inactive_1_parm.pdb
+trajin /home/jinp/project/NLRP3/inactive_no_NEK7/MD1/inactive_1_chulihou.xtc
+rms tofirst :1-894@CA,C,N,O 
+rms fit :1-558,559-894
+distance motifA-B :1-558&!@H= :559-894&!@H= out inactive_motifA-B.dat
+run
+EOD
+
+
+cpptraj -i distance.in
+rm distance.in
