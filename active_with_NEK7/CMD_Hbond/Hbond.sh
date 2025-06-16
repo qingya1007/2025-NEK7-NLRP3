90e045dd8cdf243eec4111bf5a2a1c52fe1a35e0
qingya1007 <linqingya1007@163.com>
2025-06-16 17:03:01 +0800

    Create Hbond.sh

---
+++ active_with_NEK7/CMD_Hbond/Hbond.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cat > parm.in << EOD  
+
+
+parm active_nek7_1_parm.pdb [nc]
+
+trajin active_nek7.xtc parm [nc]
+hbond HB out hbond.dat @N,H,C,CA,O series uuseries solutehb.agr avgout hbavg.dat printatomnum
+
+
+run
+# Perform lifetime analysis
+runanalysis lifetime HB[solutehb] out lifehb.dat
+
+
+EOD
+cpptraj -i parm.in
+rm parm.in
